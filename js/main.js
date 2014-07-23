441563e1e15a2e931f04d1f3c772861fa5aee2ea
Toph Tucker <tophtucker@gmail.com>
2014-07-22 21:39:28 -0400

    More cleanup, meta description

--- js/main.js
+++ js/main.js
@@ -1,111 +1,111 @@
-function loaded () {
+var quiz = [
+  { "id": 0,
+    "question": "IS YOUR BOSS A MAN?",
+    "choices": [
+      { "button": "Yes",
+        "text": "Of course. According to a 2013 Gallup poll, 54% of Americans have a male boss, versus 30% with a female one. Actually, 35% of Americans would rather work for a man, while 40% have no preference either way.",
+        "points": 1,
+        "next": 1 },
+      { "button": "No",
+        "text": "Watch out. Female bosses ￼might not help you get ahead. In 2012, MIT studied U.S. bank branches where half the managers were women. They were just as unlikely to promote underlings.",
+        "points": 1,
+        "next": 4 }
+    ]
+  },
+  { "id": 1,
+    "question": "DOES HE HAVE A WIFE?",
+    "choices": [
+      { "button": "Yes",
+        "text": "",
+        "points": 0,
+        "next": 2 },
+      { "button": "No",
+        "text": "",
+        "points": 0,
+        "next": 3 }
+    ]
+  },
+  { "id": 2,
+    "question": "DOES HIS WIFE WORK?",
+    "choices": [
+      { "button": "Yes",
+        "text": "Great! In a UNC Kenan Flagler Business School study, male managers married to working women were more likely to recommend female candidates over males for new positions.",
+        "points": 1 },
+      { "button": "No",
+        "text": "Bad news: The same research showed married men with stay-at-home wives viewed female colleagues unfavorably and had a tendency to pass them over for promotions.",
+        "points": -1 }
+    ]
+  },
+  { "id": 3,
+    "question": "DOES HE HAVE A DAUGHTER?",
+    "choices": [
+      { "button": "Yes",
+        "text": "Excellent! If a male CEO has a first child who’s a girl, a female employee’s wages will rise by 1.1%. Male employees see a much smaller rise, according to a 2012 issue of Administrative Science Quarterly.",
+        "points": 1 },
+      { "button": "No",
+        "text": "Eek. Based on the same study, when CEOs have a son, all employees’ wages drop. Fortunately, it’s less than half a percent.",
+        "points": -1 }
+    ]
+  },
+  { "id": 4,
+    "question": "ARE YOU BLOND?",
+    "choices": [
+      { "button": "Yes",
+        "text": "U. of Queensland researchers found blondes make 7% more",
+        "points": 1 },
+      { "button": "No",
+        "text": "Could you be blond?",
+        "points": -1 }
+    ]
+  },
+  { "id": 5,
+    "question": "DO YOU FLIRT AT WORK?",
+    "choices": [
+      { "button": "Yes",
+        "text": "Go for it. In 2012, Berkeley’s Haas School of Business found that women could flirt their way into higher prices when selling cars to men. In 2009, former Sec. of State Madeline Albright told Bill Maher that she flirted with foreign dignitaries so they’d concur.",
+        "points": 1 },
+      { "button": "No",
+        "text": "Safety first; flirting can backfire. A 2013 Academy of Management brief said that even though big law firms encouraged strategic flirtation, the women who tried it were looked down upon, considered stupid, and mistreated.",
+        "points": 0 }
+    ]
+  },
+  { "id": 6,
+    "question": "DO YOU DRESS UP FOR WORK?",
+    "choices": [
+      { "button": "Yes",
+        "text": "￼Well done. According to Proctor & Gamble-funded psychology research conducted at Harvard U., people think women wearing makeup look more competent and professional than those who don’t.",
+        "points": 1 },
+      { "button": "No",
+        "text": "This may be ideal. In a study titled “Intolerance of Sexy Peers,” a McMaster U. researcher discovered women were put off by other women who looked too fancy. 85% preferred a thin blond woman in khakis over the same woman in a short skirt and tall boots.",
+        "points": -1 }
+    ]
+  },
+  { "id": 7,
+    "question": "HOW’S YOUR WEIGHT?",
+    "choices": [
+      { "button": "I'm good",
+        "text": "Nice! Many studies have proven a woman’s weight has a significant effect on her earnings potential. The Journal of Applied Psychology reported in 2010 that even very thin women are punished when they gain a little.",
+        "points": 1 },
+      { "button": "I could lose a few",
+        "text": "Uh oh. There’s a sad scientific correlation: When a woman’s BMI increases, her income decreases. Her spouse’s does, too. The effect is most profound on younger women without established careers.",
+        "points": -1 }
+    ]
+  },
+  { "id": 8,
+    "question": "￼DO YOU HAVE GOOD POSTURE?",
+    "choices": [
+      { "button": "Yes",
+        "text": "A Harvard Business School study says you’ll look more powerful.",
+        "points": 1 },
+      { "button": "No",
+        "text": "Stand up straight.",
+        "points": -1 }
+    ]
+  }
+];
+var slideCount = quiz.length+2;
 
-  var quiz = [
-    { "id": 0,
-      "question": "IS YOUR BOSS A MAN?",
-      "choices": [
-        { "button": "Yes",
-          "text": "Of course. According to a 2013 Gallup poll, 54% of Americans have a male boss, versus 30% with a female one. Actually, 35% of Americans would rather work for a man, while 40% have no preference either way.",
-          "points": 1,
-          "next": 1 },
-        { "button": "No",
-          "text": "Watch out. Female bosses ￼might not help you get ahead. In 2012, MIT studied U.S. bank branches where half the managers were women. They were just as unlikely to promote underlings.",
-          "points": 1,
-          "next": 4 }
-      ]
-    },
-    { "id": 1,
-      "question": "DOES HE HAVE A WIFE?",
-      "choices": [
-        { "button": "Yes",
-          "text": "",
-          "points": 0,
-          "next": 2 },
-        { "button": "No",
-          "text": "",
-          "points": 0,
-          "next": 3 }
-      ]
-    },
-    { "id": 2,
-      "question": "DOES HIS WIFE WORK?",
-      "choices": [
-        { "button": "Yes",
-          "text": "Great! In a UNC Kenan Flagler Business School study, male managers married to working women were more likely to recommend female candidates over males for new positions.",
-          "points": 1 },
-        { "button": "No",
-          "text": "Bad news: The same research showed married men with stay-at-home wives viewed female colleagues unfavorably and had a tendency to pass them over for promotions.",
-          "points": -1 }
-      ]
-    },
-    { "id": 3,
-      "question": "DOES HE HAVE A DAUGHTER?",
-      "choices": [
-        { "button": "Yes",
-          "text": "Excellent! If a male CEO has a first child who’s a girl, a female employee’s wages will rise by 1.1%. Male employees see a much smaller rise, according to a 2012 issue of Administrative Science Quarterly.",
-          "points": 1 },
-        { "button": "No",
-          "text": "Eek. Based on the same study, when CEOs have a son, all employees’ wages drop. Fortunately, it’s less than half a percent.",
-          "points": -1 }
-      ]
-    },
-    { "id": 4,
-      "question": "ARE YOU BLOND?",
-      "choices": [
-        { "button": "Yes",
-          "text": "U. of Queensland researchers found blondes make 7% more",
-          "points": 1 },
-        { "button": "No",
-          "text": "Could you be blond?",
-          "points": -1 }
-      ]
-    },
-    { "id": 5,
-      "question": "DO YOU FLIRT AT WORK?",
-      "choices": [
-        { "button": "Yes",
-          "text": "Go for it. In 2012, Berkeley’s Haas School of Business found that women could flirt their way into higher prices when selling cars to men. In 2009, former Sec. of State Madeline Albright told Bill Maher that she flirted with foreign dignitaries so they’d concur.",
-          "points": 1 },
-        { "button": "No",
-          "text": "Safety first; flirting can backfire. A 2013 Academy of Management brief said that even though big law firms encouraged strategic flirtation, the women who tried it were looked down upon, considered stupid, and mistreated.",
-          "points": 0 }
-      ]
-    },
-    { "id": 6,
-      "question": "DO YOU DRESS UP FOR WORK?",
-      "choices": [
-        { "button": "Yes",
-          "text": "￼Well done. According to Proctor & Gamble-funded psychology research conducted at Harvard U., people think women wearing makeup look more competent and professional than those who don’t.",
-          "points": 1 },
-        { "button": "No",
-          "text": "This may be ideal. In a study titled “Intolerance of Sexy Peers,” a McMaster U. researcher discovered women were put off by other women who looked too fancy. 85% preferred a thin blond woman in khakis over the same woman in a short skirt and tall boots.",
-          "points": -1 }
-      ]
-    },
-    { "id": 7,
-      "question": "HOW’S YOUR WEIGHT?",
-      "choices": [
-        { "button": "I'm good",
-          "text": "Nice! Many studies have proven a woman’s weight has a significant effect on her earnings potential. The Journal of Applied Psychology reported in 2010 that even very thin women are punished when they gain a little.",
-          "points": 1 },
-        { "button": "I could lose a few",
-          "text": "Uh oh. There’s a sad scientific correlation: When a woman’s BMI increases, her income decreases. Her spouse’s does, too. The effect is most profound on younger women without established careers.",
-          "points": -1 }
-      ]
-    },
-    { "id": 8,
-      "question": "￼DO YOU HAVE GOOD POSTURE?",
-      "choices": [
-        { "button": "Yes",
-          "text": "A Harvard Business School study says you’ll look more powerful.",
-          "points": 1 },
-        { "button": "No",
-          "text": "Stand up straight.",
-          "points": -1 }
-      ]
-    }
-  ];
-  var slideCount = quiz.length+2;
+function loaded () {
 
   $("body").append(_.template($("#introTemplate").html()));
 
@@ -156,16 +156,16 @@ function loaded () {
        500,
        "swing"
     );
-  })
+  });
 
-  function pointText(points) {
-    if(points>5) {
-      return "You get a raise!";
-    } else if (points >= 0) {
-      return "Tread carefully. Oh, and maybe get some highlights.";
-    } else {
-      return "Have you thought about quitting your job?";
-    }
+}
+
+function pointText(points) {
+  if(points>5) {
+    return "You get a raise!";
+  } else if (points >= 0) {
+    return "Tread carefully. Oh, and maybe get some highlights.";
+  } else {
+    return "Have you thought about quitting your job?";
   }
-
 }
