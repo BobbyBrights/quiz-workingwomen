bb89b1876b114df328dff3b565428a571dcd653a
dyep49 <dyep49@gmail.com>
2014-08-04 10:22:12 -0400

    fixed bloomberg logo height problem

---
+++ server.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# thanks to jim weirich
+# run ./servefiles from the root directory
+
+require 'webrick'
+include WEBrick
+
+dir = Dir::pwd
+port = 9000
+
+
+s = HTTPServer.new(
+  :Port            => port,
+  :DocumentRoot    => dir
+)
+
+trap("INT"){ s.shutdown }
+s.start
